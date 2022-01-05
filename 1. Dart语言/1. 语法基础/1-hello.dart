f77f2ac3ed5cc2c6bdcef89f414c57d92dc285a9
lly <1776321381@qq.com>
2022-01-05 14:12:29 +0800

    feat: Dart重要

--- 1. Dart语言/1. 语法基础/1-hello.dart
+++ 1. Dart语言/1. 语法基础/1-hello.dart
@@ -1,4 +1,14 @@
+// void main() {
+//   // 第一个dart程序
+//   print('hello, world');
+// }
+
+// 定义一个函数
+void printInteger(int aNumber) {
+  print('the number is $aNumber'); // Print to console
+}
+
 void main() {
-  // 第一个dart程序
-  print('hello, world');
+  var number = 50; // declare and initialize variable
+  printInteger(number); // call a function
 }
