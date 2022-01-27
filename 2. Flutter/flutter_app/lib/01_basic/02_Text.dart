f13fcf713cc2a28436a93d767d69c292047cbaba
李留洋 <liliuyang@zhejianglab.com>
2022-01-27 17:08:11 +0800

    feat: Text文本组件

---
+++ 2. Flutter/flutter_app/lib/01_basic/02_Text.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Text'),
+        leading: const Icon(Icons.menu),
+        actions: const [
+          Icon(Icons.settings)
+        ],
+        // 阴影高度
+        elevation: 0.0,
+        centerTitle: true
+      ),
+      body: const TextDemo()
+    );
+  }
+}
+
+class TextDemo extends StatelessWidget {
+  const TextDemo({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text(
+          'FlutterFlutterFlutterFlutterFlutterFlutterFlutterFlutterFlutterFlutterFlutter',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.red,
+            fontWeight: FontWeight.w500,
+            fontStyle: FontStyle.italic,
+            decoration: TextDecoration.lineThrough,
+            decorationColor: Colors.blue
+          ),
+          textAlign: TextAlign.right,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          textScaleFactor: 1.5,
+        ),
+        RichText(
+          text: const TextSpan(
+            text: 'Hello',
+            style: TextStyle(
+              fontSize:40,
+              color: Colors.blue
+            ),
+
+            children: [
+              TextSpan(
+              text: 'F1',
+              style: TextStyle(
+                fontSize:40,
+                color: Colors.green
+              )
+            ),
+          
+          ]
+        )
+        )]
+    );
+  }
+}
